8b586f2cfcaa615af37845917956a21ac5dab343
KylChiang <chiangkaiyun@gmail.com>
2018-09-30 11:48:14 +0800

    Initial Commit

---
+++ GridPreviewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GridPreviewer
+//
+//  Created by Kyl Chiang on 2018/9/30.
+//  Copyright © 2018 KaiYun Chiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
